45c544a2ba609aeaff412c514fbb1157bb2a0c2a
dannyarena <arenadanny71@gmail.com>
2025-05-29 17:37:37 +0200

    add input field

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     
   });
   console.log(formData);
-  
+
   function handleChange(e) {
     const {name, type, value, checked } = e.target;
     const fieldValue = type === "checkbox" ? checked : value;
@@ -23,14 +23,43 @@ function App() {
     }));
   }
   return (
-    <form><label htmlFor="author">Autore</label>
+    <>
+    <form>
+      <label htmlFor="author">Autore</label>
     <input
     type="text" 
     id='author'
     name='author'
     value={formData.author}
     onChange={handleChange}/>
+
+    <label htmlFor="title">Titolo</label>
+    <input
+    type="text" 
+    id='title'
+    name='title'
+    value={formData.title}
+    onChange={handleChange}/>
+    
+
+     <label htmlFor="body">Contenuto</label>
+    <textarea
+    type="text" 
+    id='body'
+    name='body'
+    value={formData.body}
+    onChange={handleChange}/>
+
+    <label htmlFor="public">Pubblica</label>
+    <input
+    type="checkbox" 
+    id='public'
+    name='public'
+    value={formData.public}
+    onChange={handleChange}/>
     </form>
+
+    </>
   )
 }
 
