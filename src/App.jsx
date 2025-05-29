07e34bb4b1119ae7aef95f101a0f7e8f792e581f
dannyarena <arenadanny71@gmail.com>
2025-05-29 17:13:54 +0200

    create input state

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 
+const [formData, setFormData] = useState({
+author: '',
+title: '',
+body: '',
+public: false
+
+});
+console.log(formData);
+
 function App() {
 
   return (
